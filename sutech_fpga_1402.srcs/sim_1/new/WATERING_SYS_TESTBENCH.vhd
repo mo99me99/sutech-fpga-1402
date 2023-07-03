596511e775a21ddba9dfded1af28139248a44ccc
MO99ME99 <m.m.hosseini1099@gmail.com>
2023-07-03 10:30:51 -0400

    Create test bench file

---
+++ sutech_fpga_1402.srcs/sim_1/new/WATERING_SYS_TESTBENCH.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/03/2023 10:24:55 AM
+-- Design Name: 
+-- Module Name: WATERING_SYS_TESTBENCH - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity WATERING_SYS_TESTBENCH is
+--  Port ( );
+end WATERING_SYS_TESTBENCH;
+
+architecture Behavioral of WATERING_SYS_TESTBENCH is
+
+begin
+
+
+end Behavioral;
