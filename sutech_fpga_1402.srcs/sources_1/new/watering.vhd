4fba8d97d5cd95431c5fe875ee80ecee1a89907c
MO99ME99 <m.m.hosseini1099@gmail.com>
2023-07-03 07:23:08 -0400

    add WATERING entity

---
+++ sutech_fpga_1402.srcs/sources_1/new/watering.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/03/2023 05:21:51 AM
+-- Design Name: 
+-- Module Name: WATERING - WATERING_ARCH
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity WATERING is
+--  Port ( );
+end WATERING;
+
+architecture WATERING_ARCH of WATERING is
+
+begin
+
+
+end WATERING_ARCH;
