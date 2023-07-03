74de3beccc66064437b56c832fe0ad7934ccea09
MO99ME99 <m.m.hosseini1099@gmail.com>
2023-07-03 09:32:27 -0400

    add CMB:process(M_IN , L_IN , T_IN , STATE) in WATERING_FSM_ARCH

--- sutech_fpga_1402.srcs/sources_1/new/watering.vhd
+++ sutech_fpga_1402.srcs/sources_1/new/watering.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: 
+-- Engineer: Mohammad Mehdi Hosseini L
 -- 
 -- Create Date: 07/03/2023 05:21:51 AM
 -- Design Name: 
@@ -65,5 +65,37 @@ begin
         if RESET = '1' then STATE<=ST2_START;
         elsif (CLK'event and CLK = '1') then STATE<=NEXT_STATE;
         end if;
-    end process REG
+    end process REG;
+    
+    CMB:process(M_IN , L_IN , T_IN , STATE) begin
+        case STATE is 
+            -- in this case we just move to stop state because in FSM st2 goes to st0 with lambda
+            when ST2_START =>
+                NEXT_STATE <= ST0_STOP;
+                
+            when ST0_STOP => 
+                --the 2 situations that we will stay in st0 :
+                if(    ((T_IN='0' and L_IN ='0') and M_IN>"011")
+                    or ((T_IN='1' or L_IN='1') and M_IN>"001")) then
+                        NEXT_STATE <= ST0_STOP;
+                            
+                --the 2 situations that we will change our state to st1 :
+                elsif (    ((T_IN='1' or L_IN='1')and M_IN <="001")
+                    or ((T_IN='0' and L_IN ='0') and M_IN<= "011")) then 
+                        NEXT_STATE <= ST1_WATERING;
+                    end if ;
+                    
+            when ST1_WATERING =>
+                --the 2 situations that we will stay in st1
+                if(    ((T_IN='0' and L_IN='0')and M_IN<"111")
+                    or((T_IN='1' or L_IN='1') and M_IN<"011")) then
+                        NEXT_STATE <= ST1_WATERING;
+                        
+                --the 2 situations that we will change our state to st0 :
+                elsif (    ((T_IN='1' or L_IN='1')and M_IN>="011")
+                    or(M_IN>="111")) then
+                        NEXT_STATE <= ST0_STOP;
+                end if;
+        end process CMB;
+        
 end WATERING_ARCH;
