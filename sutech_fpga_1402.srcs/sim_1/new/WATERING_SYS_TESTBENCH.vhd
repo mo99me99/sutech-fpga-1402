e7c9a6c7765e23c1a88d548b09a1a45ac0193ba5
MO99ME99 <m.m.hosseini1099@gmail.com>
2023-07-03 10:40:15 -0400

    add WATERING component to WATERING_SYS_TESTBENCH

--- sutech_fpga_1402.srcs/sim_1/new/WATERING_SYS_TESTBENCH.vhd
+++ sutech_fpga_1402.srcs/sim_1/new/WATERING_SYS_TESTBENCH.vhd
@@ -1,17 +1,12 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: SUTECH
+-- Engineer: M M Hosseini L
 -- 
 -- Create Date: 07/03/2023 10:24:55 AM
--- Design Name: 
 -- Module Name: WATERING_SYS_TESTBENCH - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name: WATERING SYSTEM
+-- Target Devices: FPGAs 
+-- Description: test bench
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
@@ -22,22 +17,28 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity WATERING_SYS_TESTBENCH is
---  Port ( );
 end WATERING_SYS_TESTBENCH;
 
-architecture Behavioral of WATERING_SYS_TESTBENCH is
-
+architecture TB_WATERING_SYS_ARCH of WATERING_SYS_TESTBENCH is
+    component WATERING is
+        Port (
+            --inputs : 
+            RESET : in std_logic;
+            CLK : in std_logic;
+            M_IN : in std_logic_vector(2 downto 0);
+            L_IN : in std_logic;
+            T_IN : in std_logic;
+            
+            --outputs : 
+            STATE : out std_logic_vector(1 downto 0);
+            SEG : out std_logic_vector(6 downto 0 );
+            M_OUT : out std_logic_vector(2 downto 0);
+            L_OUT : out std_logic;
+            T_OUT : out std_logic    
+        );
+    end component WATERING;
+    
 begin
-
-
 end Behavioral;
